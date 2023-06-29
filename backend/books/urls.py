fb8ad846ef2908a7496504b0c4459eddd4305c60
spacelord16 <deshpande.aditya16@gmail.com>
2023-06-29 19:58:58 +0530

    Book Search parameter

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -4,4 +4,5 @@ from .views import BookListAPIView, BookDetailAPIView
 urlpatterns = [
     path('books/', BookListAPIView.as_view(), name='book-list'),
     path('books/<int:pk>', BookDetailAPIView.as_view(), name='book-detail'),
+    path('books/search/', BookListAPIView.as_view(), name='book-search'),
 ]
--- backend/books/views.py
+++ backend/books/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializers
+from django.db.models import Q 
 
 class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -9,3 +10,17 @@ class BookListAPIView(generics.ListAPIView):
 class BookDetailAPIView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+    
+class BookSearchAPIView(generics.ListAPIView):
+    serializer_class = BookSerializers
+    
+    def get_query(self):
+        query = self.request.query_params.get('q')  
+        if query:
+            return Book.objects.filter(
+                Q(title__icontains=query) |
+                Q(author__icontains=query) |
+                Q(genre__icontains=query)
+            )
+        else:
+            return Book.objects.all()
